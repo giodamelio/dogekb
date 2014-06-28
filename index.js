2eda0b4f84861a4eb11c1b0c53e47c1e6726dbcb
Gio d'Amelio <giodamelio@gmail.com>
2014-06-28 16:13:53 -0700

    Express hello world

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log("Hello World!");
+var express = require("express");
+
+var app = express();
+
+app.get("/", function(req, res) {
+    res.send("Hello World!");
+});
+
+app.listen(process.env.PORT || 3141);
 
