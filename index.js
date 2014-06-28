30327ae9e1baa6911891586bf3654a30206025a1
Gio d'Amelio <giodamelio@gmail.com>
2014-06-28 16:39:23 -0700

    List subdomains

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+var path = require("path");
+
 var express = require("express");
+var expressSubdomainHandler = require("express-subdomain-handler");
 
 var app = express();
 
+// Map subdomains to routes
+app.use(expressSubdomainHandler({
+    baseUrl: "localhost.com:3141",
+    prefix: "subdomain"
+}));
+
+// Handle subdomain
+app.get("/subdomain/:subdomain", function(req, res) {
+    res.send("Subdomain: " + req.params.subdomain);
+});
+
+// Homepage
 app.get("/", function(req, res) {
     res.send("Hello World!!!");
+});
+
+// Invalid subdomain
+app.get("/invalid", function(req, res) {
+    res.send("Invalid subdomain");
 });
 
 var port = Number(process.env.PORT || 3141);
